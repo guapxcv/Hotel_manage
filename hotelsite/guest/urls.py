7e99dcb6d95b6f03f530fdd67ab64e9bc0d18e2c
KSJ <fkwnaka@gmail.com>
2018-12-04 14:21:27 +0900

    url / views 수정해서 올렸습니다.

--- hotelsite/guest/urls.py
+++ hotelsite/guest/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.guest_home, name='guest_home'),
-    path('myinfo', views.guest_myinfo, name = 'guest_myinfo'),
-    path('payment', views.guest_payment, name='payment')
+    path('reservation/payment',views.payment, name='payment'),
+
 ]
